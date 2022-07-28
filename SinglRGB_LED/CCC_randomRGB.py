3039562c1942f76567b1476f9c94986cdb7f7e27
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-28 08:59:57 +0800

    Add led25 toggle for debug

--- SinglRGB_LED/CCC_randomRGB.py
+++ SinglRGB_LED/CCC_randomRGB.py
@@ -12,7 +12,9 @@ lineG.freq(1500)
 lineB = PWM(Pin(4))
 lineG.freq(2000)
 
+led25 = Pin(25, Pin.OUT)
 while True:
+    led25.toggle()
     v = 65536
     dutyR = random.randint(0,v)
     dutyG = random.randint(0,v)
@@ -20,6 +22,7 @@ while True:
     lineR.duty_u16(dutyR)
     lineG.duty_u16(dutyG)
     lineB.duty_u16(dutyB)
-    sleep(0.2)
+    sleep(1.0)
+
 
 
