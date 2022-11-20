8183704a2376c67126da59c885d8110021193d1b
milochen0418 <milo_chen@mail2000.com.tw>
2022-11-21 07:51:57 +0800

    update code and information

--- L298N_DC_motor/L298N_DC_motor.py
+++ L298N_DC_motor/L298N_DC_motor.py
@@ -2,20 +2,27 @@ from machine import Pin
 import utime
 in1 = Pin(15, Pin.OUT)
 in2 = Pin(14, Pin.OUT)
-
+led25 = Pin(25, Pin.OUT)
+delay_sec = 2
+# I need to give 5V to L298N with Brushed DC motor type B, 
+# then the Brushed DC motor can be runned by this code.
 while True:
+    led25.toggle()
     in1.value(0)
     in2.value(0)
-    utime.sleep(1)
+    utime.sleep(delay_sec)
+    led25.toggle()    
     in1.value(0)
-    in2.value(1)
-    utime.sleep(1)    
+    in2.value(0)
+    utime.sleep(delay_sec)
+    led25.toggle()    
     in1.value(1)
     in2.value(0)
-    utime.sleep(1)    
+    utime.sleep(delay_sec)
+    led25.toggle()    
     in1.value(1)
     in2.value(1)
-    utime.sleep(1)
+    utime.sleep(delay_sec)
 
     
 
