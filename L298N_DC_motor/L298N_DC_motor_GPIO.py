7576f35c07079d6202011d7df987c4964b521561
milochen0418 <milo_chen@mail2000.com.tw>
2022-11-21 07:56:43 +0800

    change sleep module

--- L298N_DC_motor/L298N_DC_motor_GPIO.py
+++ L298N_DC_motor/L298N_DC_motor_GPIO.py
@@ -1,28 +1,32 @@
 from machine import Pin
-import utime
+from time import sleep
+
 in1 = Pin(15, Pin.OUT)
 in2 = Pin(14, Pin.OUT)
 led25 = Pin(25, Pin.OUT)
+
+
 delay_sec = 2
 # I need to give 5V to L298N with Brushed DC motor type B, 
 # then the Brushed DC motor can be runned by this code.
 while True:
     led25.toggle()
     in1.value(0)
-    in2.value(0)
-    utime.sleep(delay_sec)
+    in2.value(1)
+    sleep(delay_sec)
     led25.toggle()    
     in1.value(0)
     in2.value(0)
-    utime.sleep(delay_sec)
+    sleep(delay_sec)
     led25.toggle()    
     in1.value(1)
     in2.value(0)
-    utime.sleep(delay_sec)
+    sleep(delay_sec)
     led25.toggle()    
     in1.value(1)
     in2.value(1)
-    utime.sleep(delay_sec)
+    sleep(delay_sec)
 
     
+
 
