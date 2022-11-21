bef671ba4b71e231aec72665153b9bc10641eb43
milochen0418 <milo_chen@mail2000.com.tw>
2022-11-21 08:08:08 +0800

    L298N DC motor type B :speed can be chaned by PICO PWM control now

--- L298N_DC_motor/L298N_DC_motor_PWM.py
+++ L298N_DC_motor/L298N_DC_motor_PWM.py
@@ -1,28 +1,25 @@
-from machine import Pin
-import utime
+from machine import Pin, PWM
+from time import sleep
+
+
 in1 = Pin(15, Pin.OUT)
+in1.value(0)
 in2 = Pin(14, Pin.OUT)
+in2.value(0)
+sleep(0.5)
+
+in1_pwm = PWM(Pin(15))
+in1_pwm.freq(1000)
+
 led25 = Pin(25, Pin.OUT)
 delay_sec = 2
 # I need to give 5V to L298N with Brushed DC motor type B, 
 # then the Brushed DC motor can be runned by this code.
-while True:
-    led25.toggle()
-    in1.value(0)
-    in2.value(0)
-    utime.sleep(delay_sec)
-    led25.toggle()    
-    in1.value(0)
-    in2.value(0)
-    utime.sleep(delay_sec)
-    led25.toggle()    
-    in1.value(1)
-    in2.value(0)
-    utime.sleep(delay_sec)
-    led25.toggle()    
-    in1.value(1)
-    in2.value(1)
-    utime.sleep(delay_sec)
 
-    
+while True:
+    for duty in range(0,65535,10000):
+        in1_pwm.duty_u16(duty)
+        led25.toggle()  
+        sleep(delay_sec)
+
 
